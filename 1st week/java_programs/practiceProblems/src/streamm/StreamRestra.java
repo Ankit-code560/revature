1e277b0c8ef40f9e1d45aff09f4e0affb0425e3d
Ankit <ankitsonkusare1@gmail.com>
2021-08-06 10:02:34 +0530

    done

--- 1st week/java_programs/practiceProblems/src/streamm/StreamRestra.java
+++ 1st week/java_programs/practiceProblems/src/streamm/StreamRestra.java
@@ -2,6 +2,8 @@ package streamm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.*;
 
 public class StreamRestra {
 
@@ -9,14 +11,21 @@ public class StreamRestra {
 		
 		List<RestaurantMenu> restMenu = new ArrayList<>();
 		restMenu.add(new RestaurantMenu ("Paneer Tadka",250.23,2));
-		restMenu.add(new RestaurantMenu ("dal fry",150.10,3));
+		restMenu.add(new RestaurantMenu ("Dal fry",150.10,3));
 		restMenu.add(new RestaurantMenu ("Biryani",300.75,1));
 		restMenu.add(new RestaurantMenu ("Roti",20,22));
 		restMenu.add(new RestaurantMenu ("Chicken handi",700.50,1));
 		
-		Double totalBill= restMenu.stream()
+
+		List<RestaurantMenu> sortedList = restMenu.stream()
+		        .sorted(Comparator.comparing(RestaurantMenu::getItemName))
+		        .collect(Collectors.toList());
+		
+		
+		Double totalBill= sortedList.stream()
 				.map(e -> e.getPrice()*e.getQuantity())
 				.reduce(0.0d,(s,el)-> s+el);
+		
 		
 		System.out.println("Customer Bill for Table No. 01\n");
 		
@@ -24,7 +33,7 @@ public class StreamRestra {
 		System.out.println("===============================================");
 		System.out.println("No   Name             Price    Qty.  Total ");
 		System.out.println("===============================================");
-		for( RestaurantMenu rest : restMenu) {
+		for( RestaurantMenu rest : sortedList) {
 		i++;
 		RestaurantMenu.printMenu(i,rest);
 		
